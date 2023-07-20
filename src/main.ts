43c09c0f1e2624b3ab5a2a2d18ab8c382cd92c87
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-07-20 17:48:57 +0300

    clickhouse.beta.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import clickhouse from "./clickhouse/clickhouse";
+import {clickhouse_beta} from "./clickhouse/clickhouse.beta";
 
 const data = [{
     ts: '2023-07-19 18:34:27.706',
@@ -10,9 +11,9 @@ const data = [{
     message: 'err 1311111',
 }]
 
-const lob = [{
+const lob: any[] = [{
     ts: '2023-07-19 18:34:27.706',
-    ticker: 'POLY',
+    ticker: 'mgnt',
     exchange: 'imoex',
     limitUp: 60.5,
     limitDown: 55.5,
@@ -22,23 +23,53 @@ const lob = [{
     fee: 0.5,
     diff: 0.4,
     midpoint: 0.1,
-    asks: [{ quantity: 5, price: 10.5 }, { quantity: 7, price: 16.5 }],
-    bids: [{ quantity: 1, price: 0.5 }, { quantity: 6, price: 111.5 }],
+    asks: [5, 10.5 , 7, 16.5 ],
+    bids: [5, 10.5 , 7, 16.5 ],
+    //asks: [{ quantity: 1, price: 0.5 }, { quantity: 6, price: 111.5 }],
+    //bids: [{ quantity: 1, price: 0.5 }, { quantity: 6, price: 111.5 }],
     message: 'err 1311111',
     figi: 'uuid00-1',
     isConsistent: 1,
     depth: 50
 }]
 
+async function insertData<T>(table: string, data: T[]) {
+    const values = data.map((v, i) => ({ ...v, id: i + 1 }))
+    console.log(values);
+    await clickhouse_beta.insert({
+        table,
+        values,
+        format: 'JSONEachRow',
+    })
+}
+
 const main = async () => {
-    const resp = await clickhouse.query('select now()').toPromise();
-    console.log(resp);
+    // const resp = await clickhouse.query('select now()').toPromise();
+    // console.log(resp);
+    //
+    // const result = await clickhouse.insert_rows(
+    //     'atr_cex_tinkoff_investments_v2.errors', data
+    //
+    // ).toPromise();
 
-    const result = await clickhouse.insert_rows(
-        'atr_cex_tinkoff_investments_v2.errors', data
+    console.info(await clickhouse_beta.ping())
 
-    ).toPromise();
+    await insertData('atr_cex_tinkoff_investments_v2.orderbook', lob)
 
-    console.log(result);
+    // await clickhouse_beta.insert({
+    //     table: 'atr_cex_tinkoff_investments_v2.orderbook',
+    //     // structure should match the desired format, JSONEachRow in this example
+    //     values: lob,
+    //     format: 'JSONEachRow',
+    // })
+
+
+    const resultSet = await clickhouse_beta.query({
+        query: 'select * from atr_cex_tinkoff_investments_v2.orderbook',
+        format: 'JSONEachRow',
+    })
+    const dataset = await resultSet.json()
+
+    console.log(dataset);
 }
 main().catch(error => console.error(error));
