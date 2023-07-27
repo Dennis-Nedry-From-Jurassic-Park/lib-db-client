91a05159f2ddf405b103d1dc171ff0419e879a1c
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-07-27 07:53:24 +0300

    fix

---
+++ index.ts
@@ -0,0 +1,3 @@
+export * from './src/clickhouse/clickhouse.beta';
+export * from './src/clickhouse/clickhouse';
+export * from './src/clickhouse/perform-sql';
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './clickhouse/clickhouse.beta';
-export * from './clickhouse/clickhouse';
-export * from './clickhouse/perform-sql';
