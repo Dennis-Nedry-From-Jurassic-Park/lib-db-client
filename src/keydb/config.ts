f31e3ae3ca8beafbb8c7bf81034056d4a4f08ecd
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-07-28 15:42:39 +0300

    keydb connector

---
+++ src/keydb/config.ts
@@ -0,0 +1,2 @@
+export const keydb_host = 'some-keydb-atr'
+export const keydb_port = 6379
---
+++ src/keydb/keydb.ts
@@ -0,0 +1,25 @@
+//import { createClient } from 'redis';
+
+//export const url = process.env.REDIS_URL || 'redis://localhost:6379';
+
+//console.log("url="+url);
+//process.stdout.write(`${url} is running on: http://localhost`);
+
+// export const keydb_client: any = createClient({
+//     url,
+//     legacyMode: true,
+// });
+//
+// keydb_client.on('error', (err) => console.log('KeyDB Client Error: ', err));
+//
+// keydb_client
+//     .connect()
+//     .then((it) => {
+//         //const keys = it.
+//         //console.log();
+//         console.log("url2=" + url)
+//     })
+//     .catch(err => {
+//         console.log("url="+url)
+//         console.log(err)
+//     });
