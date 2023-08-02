4c44d596b788eac42b5005cb867297cf22392fc3
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-08-02 09:51:26 +0300

    add mongodb client

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from './src/clickhouse/clickhouse.beta';
 export * from './src/clickhouse/clickhouse';
 export * from './src/clickhouse/perform-sql';
+export * from './src/mongodb/mongodb';
---
+++ src/mongodb.ts
@@ -0,0 +1,73 @@
+import {mongodb_client} from "./mongodb/mongodb";
+
+const exec = async () => {
+
+    await mongodb_client.connect().catch((err => console.log(err)));
+    console.log('Connected successfully to server');
+    const db = mongodb_client.db('atr');
+    const collection: any = db.collection('temp');
+    const findResult = await collection.find({ exchange: 'imoex' }).toArray();
+    console.log('Found documents =>', findResult);
+
+    const foods = db.collection("imoex.lob");
+
+    const docs = [
+        {
+            ts: "2023-07-19 18:34:27.706",
+            ticker: "ROSN",
+            exchange: "imoex",
+            limitUp: 60.5,
+            limitDown: 55.5,
+            bestAsk: 57.5,
+            bestBid: 51.5,
+            spread: 0.6,
+            fee: 0.5,
+            diff: 0.4,
+            midpoint: 0.1,
+            asks: [
+                { quantity: 5, price: 10.5 },
+                { quantity: 7, price: 16.5 }
+            ],
+            bids: [
+                { quantity: 1, price: 0.5 },
+                { quantity: 6, price: 111.5 }
+            ],
+            message: "err 1311111",
+            figi: "uuid00-1",
+            isConsistent: 1,
+            depth: 50
+        },
+        {
+            ts: "2023-07-19 18:34:27.906",
+            ticker: "LKOH",
+            exchange: "imoex",
+            limitUp: 4200.5,
+            limitDown: 55.5,
+            bestAsk: 57.5,
+            bestBid: 51.5,
+            spread: 0.6,
+            fee: 0.5,
+            diff: 0.4,
+            midpoint: 0.1,
+            asks: [
+                { quantity: 5, price: 10.5 },
+                { quantity: 7, price: 16.5 }
+            ],
+            bids: [
+                { quantity: 1, price: 0.5 },
+                { quantity: 6, price: 111.5 }
+            ],
+            message: "err 1311111",
+            figi: "uuid00-1",
+            isConsistent: 1,
+            depth: 50
+        }
+    ]
+
+    await db.collection('imoex.lob').insertMany(docs, { ordered: true });
+
+    //await collection.createCollection('imoex.lob')
+
+    await mongodb_client.close();
+}
+exec();
