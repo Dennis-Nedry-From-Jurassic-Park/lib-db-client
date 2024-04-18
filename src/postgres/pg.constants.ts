63af56019ce2bd3c986529b8358d51cf925e526d
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-04-18 21:43:26 +0300

    *

---
+++ src/postgres/pg.constants.ts
@@ -0,0 +1,6 @@
+export const host = "127.0.0.1:5432";
+export const user = process.env.PG_USER;
+export const schema = process.env.PG_SCHEMA;
+export const db = process.env.PG_DB;
+export const password = process.env.PG_PASSWORD;
+export const logging = false;
